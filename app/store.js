4321bfd45a84572defd554f224320db30de5d499
Stepan Sichkar <mkoty@yandex.ru>
2017-07-16 18:58:32 +0300

    update deprecated redux devToolsExtension

--- app/store.js
+++ app/store.js
@@ -3,8 +3,8 @@ import rootReducer from './reducers'
 
 export default function configureStore() {
     const finalCreateStore = compose(
-        typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ?
-            window.devToolsExtension() :
+        typeof window === 'object' && typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined' ?
+            window.__REDUX_DEVTOOLS_EXTENSION__() :
             f => f
     )(createStore);
 
